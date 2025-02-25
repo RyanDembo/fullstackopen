310140fb861c989bbdc3ff5423b5e5f9485f3314
Ryan Dembo <57205694+RyanDembo@users.noreply.github.com>
2025-02-24 20:32:16 -0500

    ignore

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 const App = () => {
-  const friends = [ 'Peter', 'Maya']
+  const [ counter, setCounter ] = useState(0)
+
+
+  const increaseByOne = () => setCounter(counter + 1)
+  
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <p>{friends}</p>
+      <div>{counter}</div>
+
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+
+      <button onClick={setToZero}>
+        zero
+      </button>
     </div>
   )
 }
