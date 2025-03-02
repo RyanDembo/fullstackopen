bdf32b803db8ee162f1fe275e8cafad932418f5f
Ryan Dembo <57205694+RyanDembo@users.noreply.github.com>
2025-03-02 17:02:04 -0500

    completed anecdotes step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,4 +1,21 @@
 import { useState } from 'react'
+import './App.css'
+
+
+const VoteButton = ({onClick, hasVoted}) => {
+  if(!hasVoted){
+    return (
+      <button onClick={onClick}>vote</button>
+    )
+  }
+  else {
+    return (
+      <span>Thank you for voting!</span>
+    )
+  }
+
+}
+
 
 const App = () => {
   const anecdotes = [
@@ -11,19 +28,61 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
-  const [selected, setSelected] = useState(0)
+   //selected = index of the current anecdote
+
+  const [selected, setSelected] = useState(0);
+  const [voteCount, setVoteCount] = useState(Array(anecdotes.length).fill(0));
+  const [hasVoted, setHasVoted] = useState(false);
+
 
   const getRandomInt = (max) => Math.floor(Math.random() * max);
 
-  const handleNextAnecdote = () => setSelected(getRandomInt(anecdotes.length));
+  const handleNextAnecdote = () => {
 
-  console.log(selected);
+    let randInt = getRandomInt(anecdotes.length);
+    while(randInt === selected) {
+      // reroll in event random number same as before
+      randInt = getRandomInt(anecdotes.length);
+    }
+    setSelected(randInt);
+    setHasVoted(false);
+  }
+  
+  const handleVote = () => {
+    const newVoteCount = [...voteCount];
+    newVoteCount[selected] += 1;
+    setVoteCount(newVoteCount);
+    setHasVoted(true);
+  }
+
+  const mostVotedQuote = () => {
+    //return index of array item with most votes
+    const currentVotes = [...voteCount];
+    let [currMaxVal,index] = [0,0];
+    
+    voteCount.forEach((element, i) => {
+      if(element >= currMaxVal){
+        index = i;
+        currMaxVal = element;
+      }
+    })
+
+    return [index, currMaxVal];
+  }
+  const [iMax , maxVal] = mostVotedQuote();
+
   return (
     <div>
+      <h1>Anecdote of the Day!</h1>
       {anecdotes[selected]}
       <br />
+      <div>has {voteCount[selected]} votes</div>
+      <VoteButton onClick={handleVote} hasVoted = {false}/>
       <button onClick={handleNextAnecdote}>next anecdote</button>
+
+      <h1>Anecdote with the most votes!</h1>
+      {anecdotes[iMax]}
+      <div>has {maxVal} votes</div>
     </div>
   )
 }
