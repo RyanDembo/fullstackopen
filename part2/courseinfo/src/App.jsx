75bc77eaf640f95dfbf7d651734099121b56162a
Ryan Dembo <57205694+RyanDembo@users.noreply.github.com>
2025-03-09 18:56:26 -0400

    2.5 completed, modularity and rendering numerous course collection

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,27 +1,50 @@
 import Course from './components/Course';
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
   // returns total number of exercises for a course
   const total = (course) => {
     const parts = [...course.parts];
@@ -30,15 +53,17 @@ const App = () => {
     return partExercises.reduce((accu, curr) => accu + curr);
   }
 
+const renderCourses = (courses) => {
+  return courses.map((course) => 
+    <div key={course.id}>
+      <Course course = {course} />
+      <strong>total of {total(course)} exercises</strong>
+    </div>
+  );
+}
 
-
-
-    console.log('total', total(course))
   return (
-    <>
-        <Course course={course} />
-        <strong>total of {total(course)} exercises</strong>
-    </>
+    renderCourses(courses)
   );
 };
 
